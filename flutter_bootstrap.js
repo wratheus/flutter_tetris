58cce6c2a9c5e82c4670a0a5e32a96e60212728b
wratheus <wratheus@users.noreply.github.com>
2026-09-04 16:34:32 +0000

    deploy: aacbcf5b760e3c198baf0521e04ec1c296e84207

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2165271640"
+    serviceWorkerVersion: "3033609882"
   }
 });
